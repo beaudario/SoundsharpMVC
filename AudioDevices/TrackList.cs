93096ca964970e2c76afecd1206e22b133b481b8
beaudario <dario.douma@upcmail.nl>
2020-10-26 14:36:06 +0100

    TrackList.cs finished for testing Added functionality for making a new , adding, removing, clearing, get, shuffle, count and get total time for TrackList

--- AudioDevices/TrackList.cs
+++ AudioDevices/TrackList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AudioDevices.Tracks
@@ -10,8 +11,77 @@ namespace AudioDevices.Tracks
     {
         private List<Track> tracks;
 
-        public TrackList();
+        public TrackList()
+        {
+            this.tracks = new List<Track>();
+        }
 
-        public TrackList(List<Track> tracks);
+        public TrackList(List<Track> tracks)
+        {
+            this.tracks = tracks;
+        }
+
+        public void Add(Track t)
+        {
+            tracks.Add(t);
+        }
+
+        public void Remove(Track t)
+        {
+            tracks.Remove(t);
+        }
+
+        public void Clear()
+        {
+            tracks.Clear();
+        }
+
+        public List<Track> GetAllTracks()
+        {
+            return tracks;
+        }
+
+        public List<Track> Shuffle()
+        {
+            List<Track> temp = new List<Track>();
+
+            foreach(var track in tracks)
+            {
+                temp.Add(track);
+            }
+
+            List<Track> shuffled = new List<Track>();
+
+            Random r = new Random();
+
+            while(temp.Count > 0)
+            {
+                int random = r.Next(temp.Count);
+                shuffled.Add(temp[random]);
+                temp.RemoveAt(random);
+            }
+
+            return shuffled;
+        }
+
+        public int Count
+        {
+            get { return tracks.Count; }
+        }
+
+        public Time TotalTime
+        {
+            get
+            {
+                int seconds = 0;
+
+                foreach(var track in tracks)
+                {
+                    seconds += track.GetTimeInSeconds();
+                }
+
+                return new Time(seconds);
+            }
+        }
     }
 }
